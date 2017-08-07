9bba3bdb7b8afef12d2242407a470c29b04355ed
Maxime Daniel <root@maxux.net>
2017-08-07 13:22:03 +0200

    Fix indentation

--- rocketchat.py
+++ rocketchat.py
@@ -7,11 +7,11 @@ class RocketChatBot():
         self.password = password
         self.server = server
         self.debug = True
-        
+
         self._prefixs = []
-        
+
         self.client = client = MeteorClient('wss://demo.rocket.chat/websocket')
-        
+
         # registering internal handlers
         self.client.on('connected', self._connected)
         self.client.on('closed', self._closed)
@@ -21,7 +21,7 @@ class RocketChatBot():
         self.client.on('changed', self._changed)
         self.client.on('unsubscribed', self._unsubscribed)
         self.client.on('subscribed', self._subscribed)
-    
+
     """
     Internal events handlers
     """
@@ -40,36 +40,36 @@ class RocketChatBot():
 
     def _added(self, collection, id, fields):
         print('[+] %s: %s' % (collection, id))
-        
+
         if not fields.get('args'):
             return
-        
+
         args = fields['args']
-        
+
         if args[0] == "GENERAL":
             print("[+] message: general, skipping")
             return
-        
+
         if args[1].get('msg'):
             return self._incoming(args[1])
-        
+
         if args[1].get('attachments'):
             return self._downloading(args[1])
-            
+
         print(args)
         print(args[0])
         print(args[1])
-        
-        
+
+
         # for key, value in fields.items():
         #    print('[+]  %s: %s' % (key, value))
 
     def _changed(self, collection, id, fields, cleared):
         print('[+] changed: %s %s' % (collection, id))
-        
+
         for key, value in fields.items():
             print('[+]  field %s: %s' % (key, value))
-            
+
         for key, value in cleared.items():
             print('[+]  cleared %s: %s' % (key, value))
 
@@ -78,7 +78,7 @@ class RocketChatBot():
 
     def _unsubscribed(self, subscription):
         print('[+] unsubscribed: %s' % subscription)
-    
+
     """
     Internal callback handlers
     """
@@ -86,7 +86,7 @@ class RocketChatBot():
         if not error:
             if self.debug:
                 print(data)
-            
+
             return
 
         print('[-] callback error:')
@@ -95,41 +95,41 @@ class RocketChatBot():
     def cb1(self, data):
         if not self.debug:
             return
-            
+
         if(data):
             print(data)
-        
+
         else:
             print("[+] callback success")
-    
-    
+
+
     """
     Internal dispatcher
     """
     def _incoming(self, data):
         print("[+] Message from %s: %s" % (data['u']['username'], data['msg']))
-        
+
         for prefix in self._prefixs:
             if data['msg'].startswith(prefix['prefix']):
                 prefix['handler'](self, data)
-    
+
     def _downloading(self, data):
         print("[+] attachement from %s: %d files" % (data['u']['username'], len(data['attachments'])))
-    
+
     """
     Public initializers
     """
     def start(self):
         self.client.connect()
         self.client.login(self.username, self.password, callback=self.cb)
-        
+
         # let's yeld to background task
         while True:
             time.sleep(3600)
-    
+
     def addPrefixHandler(self, prefix, handler):
         self._prefixs.append({'prefix': prefix, 'handler': handler})
-    
+
     def sendMessage(self, id, message):
         self.client.call('sendMessage', [{'msg': message, 'rid': id}], self.cb)
 
